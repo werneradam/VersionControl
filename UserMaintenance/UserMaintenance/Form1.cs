decd6d94c2ea906d4dcd114e0b65a98a00c30762
Werner Ádám <adam.werner@stud.uni-corvinus.hu>
2019-12-01 23:32:37 +0100

    Tetszőleges

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,6 +15,9 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.FirstName;
+            label2.Text = Resource1.LastName;
+            button1.Text = Resource1.Add;
         }
     }
 }
